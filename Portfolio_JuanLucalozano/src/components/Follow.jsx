271c22ff248864f86bb320043bca691813a31037
JuanLucaLozanoGabarron <juan-luca.lozano.gabarron@student.ehb.be>
2023-10-26 15:35:42 +0200

    Add link to social media icons

--- Portfolio_JuanLucalozano/src/components/Follow.jsx
+++ Portfolio_JuanLucalozano/src/components/Follow.jsx
@@ -2,12 +2,24 @@ import React from "react";
 import "./style/follow.css";
 import Insta from "./images/insta.png";
 import LinkedIn from "./images/linkedIn.png";
+import { Link } from "react-router-dom";
 
 export default function Follow() {
   return (
     <div className="follow">
-      <img src={LinkedIn} />
-      <img src={Insta} />
+      <Link
+        to={"https://www.linkedin.com/in/juan-luca-lozano-gabarron-37a398235/"}
+        target="_blank"
+      >
+        <img src={LinkedIn} />
+      </Link>
+      <Link
+        to={"https://www.instagram.com/lj_design.official/?hl=fr"}
+        target="_blank"
+      >
+        <img src={Insta} />
+      </Link>
+
       <p>
         Follow Me <br />
       </p>
