1fa5a23997544423c35904466647bea222586cee
JuanLucaLozanoGabarron <juan-luca.lozano.gabarron@student.ehb.be>
2023-11-07 12:08:21 +0100

    Add emailJS to my contact form

--- Portfolio_JuanLucalozano/src/routes/Contact.jsx
+++ Portfolio_JuanLucalozano/src/routes/Contact.jsx
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 import "./style/contact.css";
 import Header from "../components/Header.jsx";
 import Follow from "../components/Follow.jsx";
 import HandByHand from "./images/contact.png";
 
 export default function Contact() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_bl20e8s",
+        "template_6ohub6b",
+        form.current,
+        "51a4hwFZLyzWILxKo"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <div>
       <Header />
@@ -14,32 +37,36 @@ export default function Contact() {
       </div>
       <div className="formContact">
         <div className="form">
-          <form>
+          <form ref={form} onSubmit={sendEmail}>
             <input
               className="formInput"
               id="name"
               type="text"
               placeholder="Your name"
+              name="user_name"
             />
             <input
               className="formInput"
               id="mail"
-              type="text"
+              type="email"
               placeholder="Email address"
+              name="user_email"
             />
             <input
               className="formInput"
               id="phone"
               type="text"
               placeholder="Phone number"
+              name="phone"
             />
             <input
               className="formInput"
               id="message"
               type="text"
               placeholder="Message "
+              name="message"
             />
-            <button>
+            <button type="submit" value="Send">
               <p>Submit</p>
             </button>
           </form>
