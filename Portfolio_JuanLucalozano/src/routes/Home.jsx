83dbe8d864ea3c5964ee02f67b2b6a3c3a77c1f9
JuanLucaLozanoGabarron <juan-luca.lozano.gabarron@student.ehb.be>
2023-11-08 17:31:20 +0100

    video

--- Portfolio_JuanLucalozano/src/routes/Home.jsx
+++ Portfolio_JuanLucalozano/src/routes/Home.jsx
@@ -19,8 +19,8 @@ export default function Home() {
             onEnded={() => {
               setShowVideo(false);
             }}
-            autoPlay
             muted
+            autoPlay
             preload="none"
             playsInlinev
           >
