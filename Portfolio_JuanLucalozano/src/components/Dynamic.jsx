57829c224c6d807c4f93ecfd1154dc6cbae2eb10
JuanLucaLozanoGabarron <juan-luca.lozano.gabarron@student.ehb.be>
2023-11-28 19:18:24 +0100

    Create expert lab dynamic transition prototype component

---
+++ Portfolio_JuanLucalozano/src/components/Dynamic.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./style/dynamic.css";
+import Portfolio from "./images/portfolio.png";
+
+export default function Dynamic() {
+  return (
+    <div id="box">
+      <div id="demonstrationDynamic">
+        <img src={Portfolio} />
+      </div>
+      <div id="informationDynamic">
+        <h1>Dynamic Transition</h1>
+        <h3>Web Design & Development</h3>
+        <p>
+          For an Expert Lab project, I had to render a dynamic by adding
+          animations. So I chose to do it directly on the portfolio you're
+          visiting. Every page, button, text or image was animated.
+          <br />
+          <br />
+          To discover the animations, I'll let you browse my portfolio. <br />
+          <br />
+          Enjoy your viewing!
+        </p>
+      </div>
+    </div>
+  );
+}
